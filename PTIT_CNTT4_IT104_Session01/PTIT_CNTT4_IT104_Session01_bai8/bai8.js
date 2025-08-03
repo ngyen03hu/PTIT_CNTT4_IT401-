72d5ec6eb0db0f4fe6cd0578bc9f313be5ab7969
ducthinhit <157384683+ducthinhit@users.noreply.github.com>
2025-08-04 00:26:49 +0700

    seeesioon 01

---
+++ PTIT_CNTT4_IT104_Session01/PTIT_CNTT4_IT104_Session01_bai8/bai8.js
@@ -0,0 +1,12 @@
+const insertArrayAtPosition = (arr1, arr2, position) => {
+    if (position < 0 || position > arr1.length) {
+        console.log("Vi tri khong hop le");
+        return arr1;
+    }
+    const result = [...arr1.slice(0, position), ...arr2, ...arr1.slice(position)];
+    console.log(result);
+    return result;
+};
+
+insertArrayAtPosition([1, 2, 3, 7, 8], [4, 5, 6], 3);
+insertArrayAtPosition(['a', 'b', 'e', 'f'], ['c', 'd'], 2);
--- PTIT_CNTT4_IT104_Session01/PTIT_CNTT4_IT104_Session01_bai9/bai9.js
+++ PTIT_CNTT4_IT104_Session01/PTIT_CNTT4_IT104_Session01_bai9/bai9.js
@@ -1,6 +1,5 @@
 const a = [1, 2, 3, 5, 9];
 const b = [4, 6, 7, 8];
 const newArray = [...a, ...b];
-
 const absort = [...newArray].sort((a, b) => a - b);
 console.log(absort);
