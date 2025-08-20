bcb722c716d26ab17a60c3948ae3f2e1bb774f29
ducthinhit <157384683+ducthinhit@users.noreply.github.com>
2025-08-20 08:48:25 +0700

    seesion12

---
+++ Session12/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Ex01 from './Components/PTIT_CNTT4_IT104_Session12_bai01/Ex01'
+
+export default function App() {
+  return (
+    <>
+      <Ex01></Ex01>
+    </>
+  )
+}
---
+++ Session12/src/Components/PTIT_CNTT4_IT104_Session12_bai01/Ex01.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Ex01() {
+    return (
+        <p>
+            Nhuw
+            <h1>skhdfkshdfls</h1>
+        </p>
+    )
+}
