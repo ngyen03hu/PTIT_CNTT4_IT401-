6f21f79818cebce54238090037720b0c8d84dee2
ducthinhit <157384683+ducthinhit@users.noreply.github.com>
2025-09-03 23:54:29 +0700

    SS21

---
+++ Session21/src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import Ex01 from './Components/PTIT_CNTT4_IT104_Session21_bai01/Ex01'
+import Ex02 from './Components/PTIT_CNTT4_IT104_Session21_bai02/Ex02'
+import Ex03 from './Components/PTIT_CNTT4_IT104_Session21_bai03/Ex03'
+import Ex04 from './Components/PTIT_CNTT4_IT104_Session21_bai04/Ex04'
+import Exx05 from './Components/PTIT_CNTT4_IT104_Session21_bai05/Exx05'
+import Ex06 from './Components/PTIT_CNTT4_IT104_Session21_bai06/Ex06'
+import Ex07 from './Components/PTIT_CNTT4_IT104_Session21_bai07/Ex07'
+import Ex08 from './Components/PTIT_CNTT4_IT104_Session21_bai08/Ex08'
+function App() {
+
+  return (
+
+    <div>
+      {/* <Ex01></Ex01> <br /><hr />
+      <Ex02></Ex02> <br /><hr />
+      <Ex03></Ex03><br /><hr />
+      <Ex04></Ex04><br /><hr /> */}
+      {/* <Exx05></Exx05><br /><hr /> */}
+      {/* <Ex06></Ex06><br /><hr /> */}
+      {/* <Ex07></Ex07><br /><hr /> */}
+      <Ex08></Ex08><br /><hr />
+    </div>
+  )
+}
+
+export default App
---
+++ Session21/src/Components/PTIT_CNTT4_IT104_Session21_bai03/Ex03.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Ex03() {
+    return (
+        <div className=' flex items-center justify-center gap-8'>
+            <div className="w-[200px] h-[200px] rounded-[100px] bg-[#3b82f6] "></div>
+            <div className="w-[200px] h-[200px] rounded-[100px] bg-[#ef4444] "></div>
+            <div className="w-[200px] h-[200px] rounded-[100px] bg-[#22c55e] "></div>
+
+
+        </div>
+    )
+}
